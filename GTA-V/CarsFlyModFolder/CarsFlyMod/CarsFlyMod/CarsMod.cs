b812603abb3701e2d2fd3ff5cead310a513703c7
Lex <MyNamesLex@protonmail.com>
2022-04-01 01:15:52 +0100

    Moved mods from CarsFlyMod into a new mod called PedsEffects due to the mods not relating to cars flying and relates to Pedestrians

--- GTA-V/CarsFlyModFolder/CarsFlyMod/CarsFlyMod/CarsMod.cs
+++ GTA-V/CarsFlyModFolder/CarsFlyMod/CarsFlyMod/CarsMod.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using GTA;
 using GTA.Math;
+using NativeUI;
 using GTA.NaturalMotion;
 using GTA.Native;
-using NativeUI;
 using System.Windows.Forms;
 using System.Drawing;
 
@@ -15,48 +15,22 @@ namespace CarsFlyMod
 {
     public class CarsMod : Script
     {
+        /*
         MenuPool menuPool;
         UIMenu modMenu;
         UIMenuItem PushCarsItem;
+        */
         public bool PressedE = false; // Toggle Car Pull
         public bool PressedT = false; // Toggle Car Push
-        public bool PressedY = false; // Toggle Push All Peds
-        public bool PressedZ = false; // give all peds axe and go to player toggle
-        public bool PressedJ = false; // all peds attack player
-        public bool PressedL = false; // everything invisible
         public CarsMod()
         {
-            /*
-            modMenu = new UIMenu("Mod Menu", "Test");
-            menuPool.Add(modMenu);
-
-            PushCarsItem = new UIMenuItem("Push Cars", "Pushes Cars In Direction Your Facing");
-            modMenu.AddItem(PushCarsItem);
-            */
             Tick += OnTick;
             KeyDown += OnKeyDown;
             KeyUp += OnKeyUp;
-           // menuPool = new MenuPool();
-            //modMenu.OnItemSelect += ItemSelecterEvent;
-        }
-
-        void ItemSelecterEvent(UIMenu sender, UIMenuItem item, int index)
-        {
-            if(item == PushCarsItem)
-            {
-                PushCars();
-            }
         }
 
         public void OnTick(object sender, EventArgs e)
         {
-
-            if(menuPool != null)
-            {
-                menuPool.ProcessMenus();
-            }
-
-            Ped[] nearbyPeds = World.GetNearbyPeds(Game.Player.Character, 9999);
             Vehicle[] nearbyCars = World.GetNearbyVehicles(Game.Player.Character, 1000);
 
             //vehicle effects
@@ -65,8 +39,8 @@ namespace CarsFlyMod
             {
                 if (PressedE == true) // pull cars
                 {
-                        Vector3 distancetoplayer = Game.Player.Character.Position - v.Position;
-                        v.ApplyForce(distancetoplayer * 10);
+                    Vector3 distancetoplayer = Game.Player.Character.Position - v.Position;
+                    v.ApplyForce(distancetoplayer * 10);
                 }
 
                 else
@@ -83,77 +57,11 @@ namespace CarsFlyMod
                 {
 
                 }
-
-                if(PressedL == true) // invisible
-                {
-                    v.Opacity = 0;
-                }
-                else
-                {
-
-                }
-            }
-
-            //Ped effects
-
-            foreach(Ped p in nearbyPeds)
-            {
-                if (PressedY == true) // push peds
-                {
-                    p.ApplyForce((Game.Player.Character.ForwardVector * 9999) + (Game.Player.Character.UpVector * 9999));
-                }
-                else
-                {
-
-                }
-
-                if(PressedZ == true) // give battleaxe and go to player
-                {
-                    p.Weapons.Give(WeaponHash.BattleAxe, 1, true, true);
-                    p.Task.GoTo(Game.Player.Character);
-                    p.AlwaysKeepTask = true;
-                }
-                else
-                {
-
-                }
-
-                if(PressedJ == true) // all peds attack player
-                {
-                    p.Weapons.Give(WeaponHash.AssaultRifle, 1, true, true);
-                    p.Task.FightAgainst(Game.Player.Character);
-                    p.AlwaysKeepTask = true;
-                    p.ShootRate = 500;
-                }
-                else
-                {
-
-                }
-
-                if (PressedL == true) // invisible
-                {
-                    p.Opacity = 0;
-                    if(Game.Player.Character.IsInVehicle())
-                    {
-                        Game.Player.Character.CurrentVehicle.Opacity = 0;
-                    }
-                }
-                else
-                {
-
-                }
-            }
-
-            // player impact
-            if(PressedL == true)
-            {
-                Game.Player.Character.Opacity = 0;
             }
         }
 
         public void PushCars()
         {
-          //  Ped[] nearbyPeds = World.GetNearbyPeds(Game.Player.Character, 9999);
             Vehicle[] nearbyCars = World.GetNearbyVehicles(Game.Player.Character, 1000);
             foreach (Vehicle v in nearbyCars)
             {
@@ -163,14 +71,6 @@ namespace CarsFlyMod
 
         public void OnKeyDown(object sender, KeyEventArgs e)
         {
-            Ped[] nearbyPeds = World.GetNearbyPeds(Game.Player.Character, 9999);
-            Vehicle[] nearbyCars = World.GetNearbyVehicles(Game.Player.Character, 1000);
-
-            if(e.KeyCode == Keys.F10)
-            {
-                modMenu.Visible = !modMenu.Visible;
-            }
-
             //button toggles
 
             if (e.KeyCode == Keys.E)
@@ -195,73 +95,6 @@ namespace CarsFlyMod
                     PressedT = false;
                 }
             }
-
-            if(e.KeyCode == Keys.Y)
-            {
-                if(PressedY == false)
-                {
-                    PressedY = true;
-                }
-                else
-                {
-                    PressedY = false;
-                }
-            }
-            if (e.KeyCode == Keys.Z)
-            {
-                if (PressedZ == false)
-                {
-                    PressedZ = true;
-                }
-                else
-                {
-                    foreach (Ped p in nearbyPeds)
-                    {
-                        p.Task.WanderAround();
-                    }
-                    PressedZ = false;
-                }
-            }
-            if (e.KeyCode == Keys.J)
-            {
-                if (PressedJ == false)
-                {
-                    PressedJ = true;
-                }
-                else
-                {
-                    foreach (Ped p in nearbyPeds)
-                    {
-                        p.Task.WanderAround();
-                    }
-                    PressedJ = false;
-                }
-            }
-            if(e.KeyCode == Keys.L)
-            {
-                if(PressedL == false)
-                {
-                    PressedL = true;
-                }
-                else
-                {
-                    foreach (Ped p in nearbyPeds)
-                    {
-                        p.ResetOpacity();
-                    }
-                    foreach (Vehicle v in nearbyCars)
-                    {
-                        v.ResetOpacity();
-                    }
-                    if (Game.Player.Character.IsInVehicle())
-                    {
-                        Game.Player.Character.CurrentVehicle.ResetOpacity();
-                    }
-                    Game.Player.Character.ResetOpacity();
-                    PressedL = false;
-                }
-            }
-
         }
 
         public void OnKeyUp(object sender, KeyEventArgs e)
