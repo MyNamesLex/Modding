0a9793b708dcb77643d3c291d18c557e92cd6edf
Lex <MyNamesLex@protonmail.com>
2022-04-11 00:07:15 +0100

    SuperStrongMod

---
+++ GTA-V/SuperStrong/SuperStrong.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using System.Windows.Forms;
+
+namespace SuperStrong
+{
+    public class SuperStrong : Script
+    {
+        public SuperStrong()
+        {
+            Tick += OnTick;
+            KeyDown += OnKeyDown;
+            KeyUp += OnKeyUp;
+        }
+
+        public void OnTick(object sender, EventArgs e)
+        {
+            Entity[] entitys = World.GetNearbyEntities(Game.Player.Character.Position, 999);
+
+            for (int i = 0; i < entitys.Length; i++)
+            {
+                if (entitys[i].HasBeenDamagedBy(Game.Player.Character))
+                {
+                    Entity entity = entitys[i];
+                    entity.ApplyForce(Game.Player.Character.ForwardVector * 99);
+                }
+            }
+        }
+
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
