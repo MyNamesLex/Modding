5c2e287526045a4e76b8c1a443eeda0eb1aa9f98
Lex <MyNamesLex@protonmail.com>
2022-08-14 21:28:03 +0100

    worst driving experience mod added

---
+++ GTA-V/WorstDrivingExperience/README.md
@@ -0,0 +1,5 @@
+# Worst Driving Experience
+
+A mod that makes driving annoying in GTA-V, this mod nudges cars slightly every 5 seconds, speed limit for the player driving set at 30.
+The player can't take any damage or the player will die. The player's car explodes if it is damaged in any way. ped's driving habit is set to rushed and ped's
+car's will explode if damaged and ped's can't leave their car. All ped's hate the player.
---
+++ GTA-V/WorstDrivingExperience/WorstDrivingExperience.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+
+namespace WorstDrivingExperience
+{
+    public class WorstDrivingExperience : Script
+    {
+        public Stopwatch timer;
+        public bool StartBool = false;
+        public WorstDrivingExperience()
+        {
+            Tick += onTick;
+            KeyUp += onKeyUp;
+            KeyDown += onKeyDown;
+        }
+
+        public void Start()
+        {
+            StartBool = true;
+            timer = new Stopwatch();
+            timer.Start();
+        }
+
+        public void ResetTimer()
+        {
+            NudgeVehicles();
+            timer.Restart();
+        }
+
+        public void NudgeVehicles()
+        {
+            Vehicle[] allVehs = World.GetNearbyVehicles(Game.Player.Character, 1000);
+
+            foreach(Vehicle v in allVehs)
+            {
+                Random rx = new Random();
+                int rotx = rx.Next(-0, 0);
+                Random ry = new Random();
+                int roty = ry.Next(-0, 0);
+                Random rz = new Random();
+                int rotz = rz.Next(-0, 0);
+
+                Random fx = new Random();
+                int forcex = fx.Next(-2, 2);
+                Random fy = new Random();
+                int forcey = fy.Next(-0, 0);
+                Random fz = new Random();
+                int forcez = fz.Next(-2, 2);
+
+                Vector3 Force = new Vector3(forcex, forcey, forcez);
+                Vector3 Rot = new Vector3(rotx, roty, rotz);
+
+                if (v != null)
+                {
+                    v.ApplyForce(Force, Rot, ForceType.ForceRotPlusForce);
+                }
+            }
+        }
+
+        public void onTick(object sender, EventArgs e)
+        {
+            if(StartBool == false)
+            {
+                Start();
+            }
+
+            if (Game.Player.Character.CurrentVehicle != null)
+            {
+                Vehicle veh = Game.Player.Character.CurrentVehicle;
+                veh.MaxSpeed = 30;
+
+                if (veh.IsDamaged)
+                {
+                    veh.Explode();
+                }
+            }
+
+            if (Game.Player.Character.Health < Game.Player.Character.MaxHealth)
+            {
+                Game.Player.Character.Kill();
+            }
+
+            Ped[] allPeds = World.GetNearbyPeds(Game.Player.Character, 1000);
+            Vehicle[] allVehs = World.GetNearbyVehicles(Game.Player.Character, 1000);
+
+            foreach (Ped p in allPeds)
+            {
+                p.RelationshipGroup.SetRelationshipBetweenGroups(Game.Player.Character.RelationshipGroup, Relationship.Hate);
+                p.DrivingStyle = DrivingStyle.Rushed;
+
+                if(p.IsInVehicle() == false && p.LastVehicle != null)
+                {
+                    p.LastVehicle.IsDriveable = true;
+                    p.SetIntoVehicle(p.LastVehicle, VehicleSeat.Driver);
+                }
+
+                if(p.HasBeenDamagedByAnyWeapon())
+                {
+                    p.Kill();
+                }
+            }
+
+            foreach (Vehicle v in allVehs)
+            {
+                if (v.IsDamaged)
+                {
+                    v.Explode();
+                }
+            }
+
+            if (timer.Elapsed.TotalSeconds >= 5)
+            {
+                ResetTimer();
+            }
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
